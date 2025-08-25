eea8e53ddabd06d82e0c335249e8905ae84ef09e
madona-ashraf <madonna.ash@gmail.com>
2025-08-26 01:24:55 +0300

    madonna

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Task',
+      title: 'Flutter',
       home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
